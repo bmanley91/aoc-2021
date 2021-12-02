8c4a94921707f54bb4bb474ab35721964512bb85
Brian Manley <brian.manley@n26.com>
2021-12-02 11:44:52 -0500

    Made day2part1 test pass!

--- src/main/day2/day2.go
+++ src/main/day2/day2.go
@@ -3,9 +3,14 @@ package day2
 import (
 	"fmt"
 	"log"
+	"strconv"
+	"strings"
 
 	"manley.dev/aoc/src/main/util"
 )
+
+const directionIndex = 0
+const amountIndex = 1
 
 func Day2(path string) {
 	// Read data into array
@@ -24,7 +29,30 @@ func Part1(instructions []string) int {
 }
 
 func followInstructions(instructions []string) (depth int, horizontal int) {
-	return
+	for _, instruction := range instructions {
+		splitInstruction := strings.Split(instruction, " ")
+
+		switch splitInstruction[directionIndex] {
+		case "forward":
+			horizontal += getAmountFromInstruction(splitInstruction)
+
+		case "up":
+			depth -= getAmountFromInstruction(splitInstruction)
+
+		case "down":
+			depth += getAmountFromInstruction(splitInstruction)
+
+		default:
+			fmt.Printf("Uhhh what? %s", instruction)
+		}
+	}
+
+	return depth, horizontal
+}
+
+func getAmountFromInstruction(instruction []string) int {
+	amount, _ := strconv.Atoi(instruction[amountIndex])
+	return amount
 }
 
 func determineAnswer(depth int, horizontal int) int {
