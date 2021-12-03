8094b2b68547e8c8f3fbd5027da523ceb032f18f
Brian Manley <brian.manley@n26.com>
2021-12-03 18:15:37 -0500

    day 3 part 1 test passing

--- main/day3/day3.go
+++ main/day3/day3.go
@@ -2,6 +2,7 @@ package day3
 
 import (
 	"fmt"
+	"math"
 
 	"manley.dev/aoc/main/util"
 )
@@ -12,5 +13,58 @@ func Solve() {
 }
 
 func Part1(input []string) int {
-	return 0
+	// Loop over each input and sum up the values at each position
+	// Then divide by the number of inputs
+	// If the result is > .5, then 1 is the most common
+	// Otherwise 0 is the most common
+	// *there's nothing in the problem about what to do if there's a tie*
+	sums := make([]int, len(input[0]))
+
+	for _, reading := range input {
+		for index, char := range reading {
+			if char == '1' {
+				sums[index]++
+			}
+		}
+	}
+
+	gamma, epsilon := findGammaAndEpsilon(sums, len(input))
+
+	return gamma * epsilon
+}
+
+// Gamma binary value is the most common bits
+
+// Epsilon is the inverse of Gamma
+// (e.g. 101 => 010)
+func findGammaAndEpsilon(sumArray []int, entryCount int) (gamma int, epsilon int) {
+	mostCommon := make([]int, len(sumArray))
+	leastCommon := make([]int, len(sumArray))
+
+	for index, value := range sumArray {
+		mostCommon[index] = int(math.Round(float64(value) / float64(entryCount)))
+		if mostCommon[index] == 1 {
+			leastCommon[index] = 0
+		} else {
+			leastCommon[index] = 1
+		}
+	}
+
+	fmt.Println("Most Common ", mostCommon)
+	fmt.Println("Least Common ", leastCommon)
+
+	for i := 0; i < len(mostCommon); i++ {
+		if mostCommon[i] == 1 {
+			gamma += int(math.Pow(2, float64(len(mostCommon)-i-1)))
+		}
+
+		if leastCommon[i] == 1 {
+			epsilon += int(math.Pow(2, float64(len(mostCommon)-i-1)))
+		}
+	}
+
+	fmt.Println("Gamma: ", gamma)
+	fmt.Println("Epsilon: ", epsilon)
+
+	return gamma, epsilon
 }
