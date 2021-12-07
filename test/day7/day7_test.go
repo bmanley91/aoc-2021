6133fefa651b8d9c95ed2364c975b16ca320ba29
Brian Manley <brian.manley@n26.com>
2021-12-07 10:48:46 -0500

    finished day 7

--- test/day7/day7_test.go
+++ test/day7/day7_test.go
@@ -19,5 +19,13 @@ func TestDay7Part1(t *testing.T) {
 	util.AssertAnswer(expected, answer, t)
 }
 func TestDay7Part2(t *testing.T) {
+	expected := 168
+	// Given an input list of positions
+	positions, _ := day7.ParseInput("data/test-day-7.txt")
 
+	// When the minimum fuel cost is calculated
+	answer := day7.Part2(positions)
+
+	// The the correct response is returned
+	util.AssertAnswer(expected, answer, t)
 }
